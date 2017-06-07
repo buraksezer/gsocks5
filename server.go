9f522943691973303b019fa2dc913219cd17d691
Burak Sezer <bburaksezer@gmail.com>
2017-06-07 03:49:16 +0300

    proxy socks5 traffic over HTTP2

--- server.go
+++ server.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"crypto/tls"
+	"context"
 	"log"
 	"net"
+	"net/http"
 	"os"
 	"sync"
 	"time"
 
 	"github.com/armon/go-socks5"
+	uuid "github.com/satori/go.uuid"
 )
 
 type server struct {
 	cfg             config
+	httpServer      *http.Server
+	connStore       *connStore
 	keepAlivePeriod time.Duration
 	wg              sync.WaitGroup
 	socks5          *socks5.Server
@@ -22,8 +26,12 @@ type server struct {
 }
 
 func newServer(cfg config, sigChan chan os.Signal) *server {
+	cs := &connStore{
+		m: make(map[string]*proxyConn),
+	}
 	return &server{
 		cfg:             cfg,
+		connStore:       cs,
 		keepAlivePeriod: time.Duration(cfg.KeepAlivePeriod) * time.Second,
 		errChan:         make(chan error, 2),
 		signal:          sigChan,
@@ -33,7 +41,6 @@ func newServer(cfg config, sigChan chan os.Signal) *server {
 
 func (s *server) connSocks5(conn net.Conn) {
 	defer s.wg.Done()
-
 	ch := make(chan struct{})
 	s.wg.Add(1)
 	go func() {
@@ -48,8 +55,31 @@ func (s *server) connSocks5(conn net.Conn) {
 
 	defer close(ch)
 	if err := s.socks5.ServeConn(conn); err != nil {
-		log.Println("[ERR] gsocks5: Failed to proxy to ", conn.RemoteAddr())
+		log.Println("[ERR] gsocks5: Failed to proxy to ", conn.RemoteAddr(), err)
 	}
+}
+
+func (s *server) findSocksSocket(conn net.Conn) {
+	defer s.wg.Done()
+	buf := make([]byte, 20)
+	nr, err := conn.Read(buf)
+	if err != nil {
+		log.Println("[ERR] gsocks5: Failed to proxy to", conn.RemoteAddr(), err)
+	}
+	connID, err := uuid.FromBytes(buf[:nr])
+	if err != nil {
+		log.Println("[ERR] gsocks5: Failed to proxy to", conn.RemoteAddr(), err)
+	}
+	s.connStore.mu.Lock()
+	defer s.connStore.mu.Unlock()
+	c, ok := s.connStore.m[connID.String()]
+	if !ok {
+		log.Println("[ERR] gsocks5: Failed to proxy, not found", connID)
+		return
+	}
+	b := []byte{0}
+	conn.Write(b)
+	c.conn <- conn
 }
 
 func (s *server) serve(l net.Listener) {
@@ -66,9 +96,8 @@ func (s *server) serve(l net.Listener) {
 			s.errChan <- nil
 			return
 		}
-
 		s.wg.Add(1)
-		go s.connSocks5(conn)
+		go s.findSocksSocket(conn)
 	}
 }
 
@@ -81,6 +110,11 @@ func (s *server) shutdown() {
 	close(s.done)
 }
 
+const (
+	newSocksProxyEndpoint   = "/new-socks-proxy"
+	writeSocksProxyEndpoint = "/write-socks-proxy"
+)
+
 func (s *server) run() error {
 	host, port := s.cfg.ServerHost, s.cfg.ServerPort
 	// Create a SOCKS5 server
@@ -89,21 +123,9 @@ func (s *server) run() error {
 	if err != nil {
 		return err
 	}
-
 	s.socks5 = ss
+
 	addr := net.JoinHostPort(host, port)
-	socksAddr := net.JoinHostPort(s.cfg.ServerHost, s.cfg.ServerTLSPort)
-
-	cer, err := tls.LoadX509KeyPair(s.cfg.ServerCert, s.cfg.ServerKey)
-	if err != nil {
-		return err
-	}
-	config := &tls.Config{Certificates: []tls.Certificate{cer}}
-	tlsListener, err := tls.Listen(s.cfg.Method, socksAddr, config)
-	if err != nil {
-		return err
-	}
-
 	rawListener, err := net.Listen(s.cfg.Method, addr)
 	if err != nil {
 		return err
@@ -113,28 +135,43 @@ func (s *server) run() error {
 	s.wg.Add(1)
 	go s.serve(rawListener)
 
-	log.Println("[INF] gsocks5: TLS tunnel server runs on", socksAddr)
+	mux := http.NewServeMux()
+	s.httpServer = &http.Server{
+		Handler: mux,
+		Addr:    net.JoinHostPort(s.cfg.ServerHost, s.cfg.ServerTLSPort),
+	}
+	mux.HandleFunc(newSocksProxyEndpoint, s.newSocksProxyHandler)
+	mux.HandleFunc(writeSocksProxyEndpoint, s.writeSocksProxyHandler)
+
+	httpErr := make(chan error, 1)
 	s.wg.Add(1)
-	go s.serve(tlsListener)
+	go func() {
+		defer s.wg.Done()
+		log.Println("[INF] gsocks5: HTTP/2 server runs on", s.httpServer.Addr)
+		httpErr <- s.httpServer.ListenAndServeTLS(s.cfg.ServerCert, s.cfg.ServerKey)
+	}()
 
 	select {
 	// Wait for SIGINT or SIGTERM
 	case <-s.signal:
 	// Wait for a listener error
 	case <-s.done:
+	case hErr := <-httpErr:
+		log.Println("[ERR] gsocks5: Failed to listen HTTPS", hErr)
+
 	}
 
 	// Signal all running goroutines to stop.
 	s.shutdown()
 
+	// TODO: Check CancelFunc
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	// Shutdown HTTP server
+	s.httpServer.Shutdown(ctx)
+
 	log.Println("[INF] gsocks5: Stopping proxy", addr)
 	if err = rawListener.Close(); err != nil {
 		log.Println("[ERR] gsocks5: Failed to close listener", err)
-	}
-
-	log.Println("[INF] gsocks5: Stopping TLS tunnel server", socksAddr)
-	if err = tlsListener.Close(); err != nil {
-		log.Println("[ERR] gsocks5: Failed to close tls listener", err)
 	}
 
 	ch := make(chan struct{})
